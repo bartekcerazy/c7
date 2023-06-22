6a258b2493baf5a104ed80a7c67dc9a8fc3a5695
unknown <bartekcerazy@gmail.com>
2023-06-22 20:27:27 +0200

    Dodano pliki helloworld.py i calculator.py

---
+++ calculator.py
@@ -0,0 +1,20 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: Division by zero"
+
+print(add(5, 3))
+print(subtract(10, 4))
+print(multiply(6, 7))
+print(divide(15, 3))
+print(divide(10, 0))
